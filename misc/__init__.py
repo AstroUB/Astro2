32063145cf6f2ede0af661d74eb3f21b6ada239d
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-08-23 15:37:02 +0530

    ~misc

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,7 +1,8 @@
-from misc.helpers import edit_or_reply
+from misc.helpers import edit_or_reply, get_text
 from misc.decorators import dynamic
 
 __all__ = (
     "edit_or_reply",
+    "get_text",
     "dynamic"
 )
