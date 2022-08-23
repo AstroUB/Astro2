067e90731e3c623f00c04fabb7e106db0b4fdcd2
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-08-23 15:27:27 +0530

    ~Database

--- misc/helpers.py
+++ misc/helpers.py
@@ -2,13 +2,13 @@ import asyncio
 import math
 import os
 from pyrogram.types import Message
-
+from startup.config import SUDO
 async def edit_or_reply(msg: Message, text, parse_mode="md"):
     if not msg:
         return await msg.edit(text, parse_mode=parse_mode)
     if not msg.from_user:
         return await msg.edit(text, parse_mode=parse_mode)
-    if msg.from_user.id in Config.AFS:
+    if msg.from_user.id in SUDO:
         if msg.reply_to_message:
             kk = msg.reply_to_message.message_id
             return await msg.reply_text(
