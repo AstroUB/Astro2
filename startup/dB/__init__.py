98ed7b3fee642fda0b8b26ae8a73287aa40697c4
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-08-23 13:23:06 +0530

    Added MongoDB

---
+++ startup/dB/__init__.py
@@ -0,0 +1,27 @@
+import os
+import logging
+import os
+import time
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+from startup.config import MONGO_DB
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [FridayUB] - %(levelname)s - %(message)s",
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+
+mongodb = MongoClient(MONGO_DB)
+
+
+try:
+    mongo_client.server_info()
+except ConnectionFailure:
+    logging.error("Invalid Mongo DB URL. Please Check Your Credentials! Astro2.0 is Exiting!")
+    quit(1)
+
+
+
+dtbs = mongodb["Astro"]
