51c3261b36ddaf30385f618d3eba69d6b40cd263
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-08-23 15:28:18 +0530

    ~StartUp

--- startup/config.py
+++ startup/config.py
@@ -2,6 +2,7 @@ import os
 
 VC = os.environ.get("VC" "OFF")
 SUDO_ID = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+SUDO = list(SUDO_ID)
 HNDLR = os.environ.get("HNDLR", ".")
 DEV = set(int(x) for x in os.environ.get("DEV", "").split())
 MANAGER = os.environ.get("MANAGER", "OFF")
