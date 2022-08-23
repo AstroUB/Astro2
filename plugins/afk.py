687eaca00af43c84e43357936986a7d66dff819f
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-08-23 15:45:02 +0530

    🍭 Plugins 🍭

--- plugins/afk.py
+++ plugins/afk.py
@@ -29,16 +29,16 @@ async def set_afk(_, message: Message):
     log = Logme(message)
     if msge:
         msg = f"**{name}** \n__Going Afk Because Of😴💤__ `{msge}`"
-        await log.log_msg(client, f"#AfkLogger\n\nMaster your AFK is Activated✅\nR E A S O N: `{msge}`",)
+        await log.log_msg(astro, f"#AfkLogger\n\nMaster your AFK is Activated✅\nR E A S O N: `{msge}`",)
         go_afk(afk_start, msge)
     else:
         msg = f"**I am very Busy Right Now🥵🥵\nI can't talk to your now!!😅\n\nPlease Wait until i will come back😁**."
-        await log.log_msg(client, f"#AfkLogger Afk Is Active",)
+        await log.log_msg(astro, f"#AfkLogger Afk Is Active",)
         go_afk(afk_start) 
     await pablo.edit(msg)
         
 @dynamic(filters.mentioned & ~filters.me & ~filters.bot & ~filters.edited & filters.incoming)
-async def afk_er(client, message: Message):
+async def afk_er(astro, message: Message):
     lol = check_afk()
     if not lol:
         message.continue_propagation()
@@ -57,7 +57,7 @@ async def afk_er(client, message: Message):
     message.continue_propagation()
         
 @dynamic(filters.outgoing & filters.me)
-async def no_afke(client, message: Message):
+async def no_afke(astro, message: Message):
     name = message.from_user.mention
     lol = check_afk()
     if not lol:
@@ -70,5 +70,5 @@ async def no_afke(client, message: Message):
     await kk.delete()
     no_afk()
     log = Logme(message)
-    await log.log_msg(client, f"#AfkLogger {name} is Back Alive ! No Longer Afk\n AFK for : {total_afk_time} ")
+    await log.log_msg(astro, f"#AfkLogger {name} is Back Alive ! No Longer Afk\n AFK for : {total_afk_time} ")
     message.continue_propagation()
