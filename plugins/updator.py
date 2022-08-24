e37218425161be0b380f95b0341eff16288a593a
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-08-24 09:30:30 +0530

    🍫Updater🔺

--- plugins/updator.py
+++ plugins/updator.py
@@ -134,7 +134,7 @@ async def updating(_, msg: Message):
             try: 
               await astro.send_message(msg.chat.id, "Bot Updated Successfully!\n\n~Enjoy🥰✨")
             except BaseException as err:
-              await bot.send_message(f"You have got a Error While Updating {err}\n\nForward to @Astro_HelpChat\nFor Help😣")
+              await astro.send_message(msg.chat.id, f"You have got a Error While Updating {err}\n\nForward to @Astro_HelpChat\nFor Help😣")
         except BaseException as error:
             return await msg.edit(f"**Updater Error** \nTraceBack : `{error}`")
         await msg.edit("`Build Started! Please Wait For 10-15 Minutes!`")
