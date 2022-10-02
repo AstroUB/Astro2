be3d783bede1ea9776ea037205be7b2b35dc79a5
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2022-10-02 15:04:48 +0530

    Soon update

--- plugins/alive.py
+++ plugins/alive.py
@@ -4,13 +4,37 @@
 
 
 from . import *
+import datetime
 from plugins import vision, python
 from startup.config import ALV_PIC, ALV_TXT
+
+async def get_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
 
 
 @astro.on_message(filters.command("alive", HNDLR))
 async def zindahu(astro, msg: Message):
     user = await msg.from_user.mention
+    start = datetime.datetime.now()
+    uptime = await get_time((time.time() - start_time))
     text = f"""
 
         {ALV_TXT}
